e4f4d5d56bc424290adccbe5f86cce03631e48c0
Fabien Proriol <fabien.proriol@viavisolution.com>
2018-10-03 16:34:12 +0200

    Decode query result

--- test/query.py
+++ test/query.py
@@ -16,4 +16,26 @@ with Socket(REQ) as socket:
     query = " ".join(sys.argv[1:])
     print(query)
     socket.send(query)
-    print(socket.recv())
+    res = socket.recv()
+    if res[0] != 0x62:
+        print(res.decode("utf-8"))
+    else:
+        if res[1] == 1:
+            # Signed
+            val = struct.unpack('<q', res[2:])
+            print(val[0])
+        elif res[1] == 2:
+            # Unsigned
+            val = struct.unpack('<Q', res[2:])
+            if query.startswith("gad?"):
+                gad = val[0]
+                print("%d/%d/%d"%((gad >> 11) & 0x1f, (gad >> 8) & 0x07, (gad) & 0xff));
+            else:
+                print(val[0])
+        elif res[1] == 3:
+            # Float
+            val = struct.unpack('<d', res[2:])
+            print(val[0])
+        else:
+            # UNKNOWN
+            print("undef")
